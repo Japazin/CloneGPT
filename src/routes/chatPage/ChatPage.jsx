2fc65a0c43bb1bf9bc7fd8da6d933a276eec2b82
MATEUS FELIPE ('JAPÃO') <mateusfma2bit@gmail.com>
2024-12-13 09:48:38 -0300

    ok

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,5 +1,38 @@
+import "./chatPage.css"
+
 const ChatPage = () => {
-  return <div className="chatPage">ChatPage</div>;
+  return (
+    <div className="chatPage">
+      <div className="waapper">
+        <div className="chat">
+          <div className="message">Test message from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test nessage from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test nessage from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test nessage from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test nessage from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test nessage from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ia</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test nessage from ia</div>
+          <div className="message user">Test message from user</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
