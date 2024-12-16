06153a1e42130038ad65a79e4e6edde4dbde137f
MATEUS FELIPE ('JAPÃO') <mateusfma2bit@gmail.com>
2024-12-16 10:28:36 -0300

    ok

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -3,10 +3,14 @@ import "./chatPage.css"
 const ChatPage = () => {
   return (
     <div className="chatPage">
-      <div className="waapper">
+      <div className="wrapper">
         <div className="chat">
           <div className="message">Test message from ia</div>
-          <div className="message user">Test message from user</div>
+          <div className="message user">Test message from user Lorem, 
+            ipsum dolor sit amet consectetur adipisicing elit.
+             Optio odit aspernatur quasi iure dolore molestiae quas?
+              Labore, ad delectus dolorem consectetur odit iure nemo itaque, 
+              cupiditate voluptatibus libero iusto fugit! </div>
           <div className="message">Test nessage from ia</div>
           <div className="message user">Test message from user</div>
           <div className="message">Test message from ia</div>
@@ -24,11 +28,7 @@ const ChatPage = () => {
           <div className="message">Test message from ia</div>
           <div className="message user">Test message from user</div>
           <div className="message">Test nessage from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test nessage from ia</div>
-          <div className="message user">Test message from user</div>
+          
         </div>
       </div>
     </div>
