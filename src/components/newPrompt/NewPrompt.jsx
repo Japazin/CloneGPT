a2898520500ec2c452f5b4bcfc959ff2c5e47915
MATEUS FELIPE ('JAPÃO') <mateusfma2bit@gmail.com>
2024-12-19 14:47:17 -0300

    ok

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -3,7 +3,18 @@ import "./newPrompt.css";
 
 const NewPrompt = () => {
   return (
-    <div className='newPrompt'>NewPrompt</div>
+    <div className="newPrompt">
+      <form action="newForm">
+        <label htmlFor="file">
+          <img src="/attachment.png" alt=""/>
+        </label>
+        <input id="file" type="file" multiple={false} hidden/>
+        <input type="text" placeholder="Ask Anything..." />
+        <button>
+        <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+    </div>
   )
 }
 
