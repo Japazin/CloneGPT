e02b4a1ecf7df34d4bee7c1834fa0a7ddf9cfdc3
MATEUS FELIPE ('JAPÃO') <mateusfma2bit@gmail.com>
2024-12-26 14:13:42 -0300

    ok

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -1,11 +1,17 @@
+import { useEffect, useRef } from "react";
 import "./newPrompt.css";
 
 
 const NewPrompt = () => {
+  const endRef = useRef(null);
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   return (
     <>
-     {/* ADD NEW CHAT */}
-      <div className="endChat"></div>
+      {/* ADD NEW CHAT */}
+      <div className="endChat" ref={endRef}></div>
       <form className="newForm">
         <label htmlFor="file">
           <img src="/attachment.png" alt=""/>
--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,13 +1,7 @@
-import { useRef } from "react";
 import "./chatPage.css";
-import { useEffect } from "react";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
-  const endRef = useRef(null);
-  useEffect(() => {
-    endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
   return (
     <div className="chatPage">
       <div className="wrapper">
@@ -36,7 +30,7 @@ const ChatPage = () => {
           <div className="message">Test message from ia</div>
           <div className="message user">Test message from user</div>
           <div className="message">Test nessage from ia</div>
-          <NewPrompt />
+          <NewPrompt /> 
         </div>
       </div>
     </div>
