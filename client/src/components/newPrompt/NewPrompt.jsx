63bc77f32afaa5167fbbb75e463e4cf9725a6578
MATEUS FELIPE ('JAPÃO') <mateusfma2bit@gmail.com>
2025-01-03 12:15:43 -0300

    API gemini ok

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
 import Upload from "../upload/upload";
 import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
 
 const NewPrompt = () => {
   const [img, setImg] = useState({
@@ -15,6 +16,14 @@ const NewPrompt = () => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
 
+  const add = async (e) => {
+    const prompt = "em que ano o brasil foi descoberto?";
+
+    const result = await model.generateContent(prompt);
+
+    console.log(result.response.text());
+  };
+
   return (
     <>
       {/* ADD NEW CHAT */}
@@ -23,8 +32,11 @@ const NewPrompt = () => {
         <IKImage
           urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
           path={img.dbData?.filePath}
+          width={380}
+          transformation={[{ width: 380 }]}
         />
       )}
+      <button onClick={add}>TESTE IA</button>
       <div className="endChat" ref={endRef}></div>
       <form className="newForm">
         <Upload setImg={setImg} />
