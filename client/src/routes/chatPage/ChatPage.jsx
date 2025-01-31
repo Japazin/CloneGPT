35155179a62fec31f5e28f5b76ed53154ae1dc13
MATEUS FELIPE ('JAPÃO') <mateusfma2bit@gmail.com>
2025-01-31 13:33:46 -0300

    read chat

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,36 +1,34 @@
 import "./chatPage.css";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation } from "react-router";
+import Markdown from "react-markdown";
 
 const ChatPage = () => {
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
+  const { isPending, error, data } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
   return (
     <div className="chatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message">Test message from ia</div>
-          <div className="message user">
-            Test message from user Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Optio odit aspernatur quasi iure dolore molestiae
-            quas? Labore, ad delectus dolorem consectetur odit iure nemo itaque,
-            cupiditate voluptatibus libero iusto fugit!{" "}
-          </div>
-          <div className="message">Test nessage from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test nessage from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test nessage from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test nessage from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test message from ia</div>
-          <div className="message user">Test message from user</div>
-          <div className="message">Test nessage from ia</div>
-          <NewPrompt /> 
+          {isPending
+            ? "loading..."
+            : error
+            ? "Something went wrong!"
+            : data?.history?.map((message, i) => (
+                <div className="message user" key={i}>
+                  <Markdown>{message.parts[0].text}</Markdown>
+                </div>
+              ))}
+
+          <NewPrompt />
         </div>
       </div>
     </div>
